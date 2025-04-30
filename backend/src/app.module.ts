5e5e627afaa00c56df591ae7275b7fe1fee60b1c
franciscoplaza <franciscoplaza99@gmail.com>
2025-04-30 19:59:53 -0400

    Revert "borrar cosas"
    This reverts commit a1dc121b5e3851790ee62cc03ca9b5c9a4018ebb.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,21 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
+import { Usuario, UsuarioSchema } from './usuario/usuario.schema';
+import { Cancha, CanchaSchema } from './cancha/cancha.schema';
+import { Reserva, ReservaSchema } from './reserva/reserva.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // Carga variables del .env
     MongooseModule.forRoot(process.env.MONGO_URI!), // el ! le dice a TS que no es undefined - Conexión a MongoDB
     // Aquí luego podés importar tus módulos, como ReservaModule, CanchaModule, etc.
+
+    MongooseModule.forFeature([
+      { name: Usuario.name, schema: UsuarioSchema },
+      { name: Cancha.name, schema: CanchaSchema },
+      { name: Reserva.name, schema: ReservaSchema },
+    ]),
     
   ],
   controllers: [AppController],
