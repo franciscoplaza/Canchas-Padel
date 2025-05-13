2cdfcb4d1eeefd46cc5097cafc91f311e966fb3c
FelipeBlancoP <felipe.blanco@alumnos.ucn.cl>
2025-05-13 01:02:10 -0400

    estilo adminreservas y mis reservas

--- frontend/src/components/MisReservas.tsx
+++ frontend/src/components/MisReservas.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import './MisReservas.css';
 interface Reserva {
   _id: string;
   fecha_hora: string;
