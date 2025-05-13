1ee7f20430c40454af118bf14de2df53a6f4f8e7
franciscoplaza <franciscoplaza99@gmail.com>
2025-05-12 21:23:11 -0400

    inicio sesión exitoso

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,22 +1,29 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { UsuarioModule } from '../usuario/usuario.module'; // Asegúrate de importar el módulo de usuario
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Importa ConfigModule y ConfigService
+import { UsuarioModule } from '../usuario/usuario.module';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller'; // Si tienes un controlador para autenticación
+import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy'; // Asegúrate de crear esta estrategia
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    forwardRef(() => UsuarioModule), // Importa el módulo de usuario para acceder a UsuarioService
-    PassportModule.register({ defaultStrategy: 'jwt' }), // Configura Passport para usar JWT
-    JwtModule.register({
-      secret: process.env.JWT_SECRET, // Asegúrate de definir esto en tu archivo .env
-      signOptions: { expiresIn: '60s' }, // Puedes ajustar el tiempo de expiración
+    forwardRef(() => UsuarioModule),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // Configuración asíncrona de JwtModule usando ConfigService
+    JwtModule.registerAsync({
+      imports: [ConfigModule], // Necesario para inyectar ConfigService
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // Obtiene el secreto de .env
+        signOptions: { expiresIn: '60s' }, // Opciones adicionales
+      }),
+      inject: [ConfigService], // Inyecta ConfigService en useFactory
     }),
+    ConfigModule.forRoot(), // Asegura que las variables de entorno estén cargadas
   ],
-  providers: [AuthService, JwtStrategy], // Proveedores necesarios
-  controllers: [AuthController], // Si tienes un controlador para manejar rutas de autenticación
-  exports: [AuthService, PassportModule, JwtModule], // Exporta AuthService y PassportModule para usarlos en otros módulos
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService, PassportModule, JwtModule],
 })
 export class AuthModule {}
