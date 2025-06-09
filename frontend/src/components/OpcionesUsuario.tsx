f71ae65cb0602a653535a5c22d8fa65506c2b784
Garcia-IGC <ignacio.garcia02@alumnos.ucn.cl>
2025-06-09 10:37:19 -0400

    Boton para acceder al saldo desde el menu de opciones
    wuejeje

--- frontend/src/components/OpcionesUsuario.tsx
+++ frontend/src/components/OpcionesUsuario.tsx
@@ -81,6 +81,40 @@ const OpcionesUsuario = () => {
               </button>
             </div>
           </div>
+
+          <div className="user-card">
+            <div className="card-icon reservas-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                <line x1="16" y1="2" x2="16" y2="6"></line>
+                <line x1="8" y1="2" x2="8" y2="6"></line>
+                <line x1="3" y1="10" x2="21" y2="10"></line>
+              </svg>
+            </div>
+            <div className="card-content">
+              <h2 className="card-title">Saldo</h2>
+              <p className="card-description">
+                Cargar Saldo
+              </p>
+              <button 
+                onClick={() => navigate("/saldo")} 
+                className="user-btn reservas-btn"
+              >
+                Ver mi Saldo
+              </button>
+            </div>
+          </div>
+
         </div>
 
         <div className="user-actions">
